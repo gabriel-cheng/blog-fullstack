465d8df7fd51f7428679903cc65cca73d8d619e3
Gabriel Carvalho <gabriel_carvalho.contato@outlook.com>
2022-11-03 00:09:14 -0300

    :tada: feat - criando estrutura inicial :fire:

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+    <main>
+        <h1>Você está na Home!</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus nulla similique, eveniet totam nemo natus quibusdam sed dolore est ut eligendi culpa consequatur laborum, quisquam sunt temporibus optio molestias earum!
+        </p>
+    </main>
+</template>
