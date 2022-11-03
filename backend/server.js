12974b57b5e5331b507f8353af3e2a9a7cd7e29e
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2022-11-03 15:57:00 -0300

    :sparkles: chore - adicionando configuração de cors :fire:

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const router = require('./router/routes.js');
 const mongoConnect = require('./database/connect');
+
 app.use(express.json());
+app.use(cors());
 
 mongoConnect();
 
